105296b82cd04ef8c14376f6bc478b393833862b
chrisbrasington <chrisbrasington@gmail.com>
2015-04-20 21:36:32 -0600

    basic get + authentication

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import requests, pprint
 from keys import api
+
+def get(url):
+
+	response = requests.get(url, auth=(api.username, api.password))
+	data = response.json()
+
+	pprint.pprint(data)
 
 if __name__ == '__main__':
 
 	print 'hello world'
+	url = 'https://www.tunefind.com/api/v1/show/supernatural/season-1/5239'
+
+	get(url)
+
+
+
+
