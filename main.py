df0497c46cbef79d8f230d3238cc6d5de8f03d89
chrisbrasington <chrisbrasington@gmail.com>
2015-04-21 21:57:37 -0600

    working full implementation

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-import requests, pprint, json, time
-from keys import api
+import requests, json, sys, getpass
+from gmusicapi import Mobileclient
+# must load tunefind api keys
+try:
+	from keys import tunefind
+except:
+	print 'unable to load tunefind keys'
+	sys.exit()
+try:
+	from keys import google
+except:
+	print '',
 
-# debug output
-def debug(text):
-	pprint.pprint(text)
+api = Mobileclient()
 
 # get json
 def get(url):
-	print
-	print url
-	response = requests.get(url, auth=(api.username, api.password))
+	response = requests.get(url, auth=(tunefind.username, tunefind.password))
 	data = response.json()
 	return data
 
@@ -17,47 +23,95 @@ def get(url):
 def get_show(show):
 
 	# get show's season information
-	base_url = 'https://www.tunefind.com/api/v1/show/' #supernatural/season-1/5239
-	url = base_url + show
-	data = get(url)
+	base_url = 'https://www.tunefind.com/api/v1/show/'
+	url = base_url + show 
+	return get(url)
 
-	# count the number seasons
-	seasons = int(data['seasons'][-1]['number'])
+# get individual song ID for google musc
+def get_song_id(song):
+	result = api.search_all_access(song,10)
 
-	# for each season
-	for season in range(1,seasons+1):
-		season_url = url + '/season-' + str(season)
-		data = get(season_url)
+	# take first song hit
+	if len(result['song_hits']) > 0:
+		found_song = result['song_hits'][0]
+		id = found_song['track']['nid']
+		print '\t', song
+		return id
+	else:
+		return False
 
-		# get each episode id
-		episodes = []
-		for episode in data['episodes']:
-			print episode['name'], '(', episode['id'], ')', ' songs: ', episode['song_count']
-			episodes.append(episode['id'])
+# authenticate with google music
+def login():
+	global api
 
-		raw_input('\nEnter to continue..')
+	logged_in = False
 
-		# for each episode, get songs
-		for episode in episodes:
-			data = get(season_url+'/'+episode)
+	# ask user for credentials if not loaded
+	if 'google' not in globals():
+		username = raw_input('google email: ')
+		password = getpass.getpass('password: ')
+		logged_in = api.login(username, password)	
+	else:
+		logged_in = api.login(google.username, google.password)
 
-			for song in data['songs']:
-				print song['name'], '-', song['artist']['name'], '(', song['confidence'], ')'
-			print
-		break
-
+	if logged_in:
+		return True
+	else:
+		print 'unable to login to google music'
+		return False
+	
 # main
 if __name__ == '__main__':	
 
-	# test show
-	get_show('supernatural')
+	# input show name
+	show = raw_input('Input show: ').lower()
+
+	# authenticate to google music
+	if login():
+		
+		# create playlist
+		playlist_id = api.create_playlist(show)
+
+		# load seasons in show
+		data = get_show(show)
+		
+		# song id array (added per season to single playlist)
+		song_ids = []
+
+		# for each season
+		for season in data['seasons']:
+			season_url = season['tunefind_api_url']
+			season_number = season['number']
+
+			print show + ' Season: ' + season_number, '\n'
+
+			data = get(season_url)
+
+			# for each episode
+			for episode in data['episodes']:
+				
+				data = get(season_url+'/'+episode['id'])
+
+				# mismatch between sound count and data returned by API?
+				print episode['name'], ' songs: ', len(data['songs']), '/', episode['song_count']
+
+				# for each song
+				for song in data['songs']:
+					if(song['confidence'] == 'high'):
+						search_song = song['artist']['name'] + ' - ' + song['name']
+
+						# get song ID
+						id = get_song_id(search_song)
+						if(id):
+							song_ids.append(id)
+						#else:
+						#	print search_song + ' (not found)'
+				print
+				
+			# add season of music to playlist 
+			print 'Adding to playlist: ' + show
+			api.add_songs_to_playlist(playlist_id, song_ids)
+
+			# clear song list
+			song_ids[:] = []
 	
-	# user input
-	'''
-	show = ''
-	while show != 'quit':
-		show = raw_input('Input show: ')
-		get_show(show)
-	'''
-
-
